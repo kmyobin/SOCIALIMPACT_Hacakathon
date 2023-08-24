cc339b8b7870bec31f2feb16e5cefb595dded53c
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-08-24 19:59:31 +0900

    kmyobin commit

--- src/components/enterprise/EnterpriseContent.jsx
+++ src/components/enterprise/EnterpriseContent.jsx
@@ -2,84 +2,88 @@ import React, { useRef } from 'react'
 import Footer from '../../layout/Footer';
 import ex_intro from "../../assets/ex_intro.mp4";
 import bag from "../../assets/images/icons/ent.svg";
+import AcceptBtn from '../../common/AcceptBtn';
+import list_5 from "../../assets/images/icons/list_5.svg"
+import ent_logo from "../../assets/images/icons/ent_logo.svg"
 
 function EnterpriseContent() {
   return (
     <div className="flex h-full w-full flex-wrap justify-center">
-      <div className="relative flex h-full w-full max-w-lg flex-col justify-between overflow-hidden bg-[#ECEFF4]">
-        {/*<div className="h-full w-full overflow-y-auto p-10">*/}
-        <div className="w-full h-[250px] ">
+      <div className="relative flex h-full w-full max-w-lg flex-col justify-between overflow-hidden bg-[#f6f6f6]">
+        <div className="w-full h-[210px] z-0">
           <video preload="metadata" controls className="w-screen">
             <source src={ex_intro} type="video/mp4" />
           </video>
         </div>
-        <div className="p-10 w-full h-full my-3">
+
+        <div className="rounded-t-xl px-8 pt-10 w-full h-full bg-[#F6F6F6] z-40 relative">
+          <div className='z-50 absolute right-12 -top-9'>
+            <img src={ent_logo} alt="ent_logo" width={80} height={80}/>
+          </div>
           <h1>
             <span className="text-xl font-bold">권태훈</span>&nbsp;(68세)
           </h1>
-          <div className="text-[#505050] text-sm py-1">
+          <div className="text-[#505050] text-xs py-1 pb-5">
             인천광역시 미추홀구 용현1동
           </div>
-          <div className="rounded-lg bg-[#D9D9D9] p-5 mt-5  min-h-[230px] ">
-            <div className="py-2">
-              <div className="flex">
-                <div className="w-1/4 flex justify-center items-center">
-                  <img
-                    src={bag}
-                    alt="bag"
-                    width={30}
-                    height={30}
-                    className="rounded-full "
-                  />
+          <div className="rounded-lg py-5 px-3 min-h-[230px] ">
+            <div className="rounded-xl shadow-md shadow-slate-500 mb-5">
+              <div className="py-2">
+                <div className="flex">
+                  <div className="w-1/4 flex justify-center items-center">
+                    <img
+                      src={list_5}
+                      alt="list_5"
+                      width={30}
+                      height={30}
+                      className="rounded-full "
+                    />
+                  </div>
+                  <div className="w-3/4  flex items-center">교육업, 일본어</div>
                 </div>
-                <div className="w-3/4  flex items-center">교육업 3년</div>
               </div>
             </div>
-            <div className="py-2">
-              <div className="flex">
-                <div className="w-1/4 flex justify-center items-center">
-                  <img
-                    src={bag}
-                    alt="bag"
-                    width={30}
-                    height={30}
-                    className="rounded-full"
-                  />
+            <div className="rounded-xl shadow-md shadow-slate-500 mb-5">
+              <div className="py-2">
+                <div className="flex">
+                  <div className="w-1/4 flex justify-center items-center">
+                    <img
+                      src={bag}
+                      alt="bag"
+                      width={30}
+                      height={30}
+                      className="rounded-full"
+                    />
+                  </div>
+                  <div className="w-3/4  flex items-center">교직 18년</div>
                 </div>
-                <div className="w-3/4  flex items-center">일본어 강사 3년</div>
               </div>
             </div>
-            <div className="py-2">
-              <div className="flex">
-                <div className="w-1/4 flex justify-center items-center">
-                  <img
-                    src={bag}
-                    alt="bag"
-                    width={30}
-                    height={30}
-                    className="rounded-full"
-                  />
+            <div className="rounded-xl shadow-md shadow-slate-500 mb-5">
+              <div className="py-2">
+                <div className="flex">
+                  <div className="w-1/4 flex justify-center items-center">
+                    <img
+                      src={bag}
+                      alt="bag"
+                      width={30}
+                      height={30}
+                      className="rounded-full"
+                    />
+                  </div>
+                  <div className="w-3/4  flex items-center">
+                    일본어 강사 5년
+                  </div>
                 </div>
-                <div className="w-3/4  flex items-center">정년퇴직</div>
               </div>
             </div>
-            <div className="py-2">
-              <div className="flex">
-                <div className="w-1/4 flex justify-center items-center">
-                  <img
-                    src={bag}
-                    alt="bag"
-                    width={30}
-                    height={30}
-                    className="rounded-full"
-                  />
-                </div>
-                <div className="w-3/4  flex items-center">OO요양원 업무 2년</div>
-              </div>
+          </div>
+          <div className="pt-4">
+            <div className="flex justify-end">
+              <AcceptBtn text={"연락하기"} />
             </div>
           </div>
         </div>
-        {/*</div>*/}
         <Footer />
       </div>
     </div>
--- src/components/history/HistoryContent.jsx
+++ src/components/history/HistoryContent.jsx
@@ -2,54 +2,93 @@ import React from 'react'
 import Footer from '../../layout/Footer';
 import profile from "../../assets/images/profile.jpg"
 import profile2 from "../../assets/images/profile_2.png"
-
+import arrow from "../../assets/images/icons/before_arrow.svg"
+import next_arrow from "../../assets/images/icons/next_arrow.svg"
 function HistoryContent() {
+    const onClickArrow = () => {
+      console.log("뒤로가기");
+    };
   return (
     <div className="flex h-full w-full flex-wrap justify-center">
-      <div className="relative flex h-full w-full max-w-lg flex-col justify-between overflow-hidden bg-[#ECEFF4]">
-        <div className="h-full w-full overflow-y-auto p-10">
-          <h1 className="font-black text-xl">조회 기록</h1>
-          <div className="py-3">
-            <div className="rounded-lg bg-[#D9D9D9] p-3 my-5">
-              <div className="flex">
-                <div className="w-1/4">
-                  <img
-                    src={profile}
-                    alt="profile"
-                    className="rounded-full"
-                    width={70}
-                    height={70}
-                  />
-                </div>
-                <div className="w-3/4 py-3">
-                  <div>
-                    <span className="text-lg font-extrabold">차은우</span>
-                    &nbsp;(80세)
+      <div className="relative flex h-full w-full flex-col justify-between overflow-hidden bg-[#f6f6f6]">
+        <div className="h-full w-full overflow-hidden bg-[#5BBDF4]">
+          <div className="bg-[#5BBDF4] w-full px-7 flex py-4">
+            <div className="w-1/4 flex justify-start items-center">
+              <img
+                src={arrow}
+                alt="arrow"
+                width={12}
+                height={12}
+                className="cursor-pointer"
+                onClick={onClickArrow}
+              />
+            </div>
+            <div className="text-xl w-1/2 flex justify-center items-center text-white text-center font-semibold">
+              조회 기록
+            </div>
+          </div>
+          <div
+            className="h-full w-full overflow-hidden py-5 px-4 bg-[#f6f6f6] rounded-t-2xl "
+            style={{ boxShadow: "0px -3px 0px 0px rgba(0,0,0,0.1)" }}
+          >
+            <div>
+              <div className="bg-white rounded-xl p-3 my-5">
+                <div className="flex">
+                  <div className="w-1/4 flex justify-center items-center">
+                    <img
+                      src={profile}
+                      alt="profile"
+                      className="rounded-full"
+                      width={60}
+                      height={60}
+                    />
                   </div>
-                  <div className=" text-[#505050] text-sm">
-                    서울특별시 광진구
+                  <div className="w-2/4 p-3">
+                    <div>
+                      <span className="text-lg font-extrabold">차은우</span>
+                      &nbsp;(80세)
+                    </div>
+                    <div className=" text-[#505050] text-sm">
+                      서울특별시 광진구
+                    </div>
+                  </div>
+                  <div className="w-1/4 flex justify-end">
+                    <img
+                      src={next_arrow}
+                      alt="next_arrow"
+                      width={15}
+                      height={15}
+                    />
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="rounded-lg bg-[#D9D9D9] p-3">
-              <div className="flex">
-                <div className="w-1/4">
-                  <img
-                    src={profile2}
-                    alt="profile2"
-                    className="rounded-full"
-                    width={70}
-                    height={70}
-                  />
-                </div>
-                <div className="w-3/4 py-3">
-                  <div>
-                    <span className="text-lg font-extrabold">권태훈</span>
-                    &nbsp;(68세)
+              <div className="bg-white rounded-xl p-3 my-5">
+                <div className="flex">
+                  <div className="w-1/4 flex justify-center items-center">
+                    <img
+                      src={profile2}
+                      alt="profile2"
+                      className="rounded-full"
+                      width={60}
+                      height={60}
+                    />
                   </div>
-                  <div className=" text-[#505050] text-sm">
-                    인천광역시 미추홀구 용현1동
+                  <div className="w-2/4 p-3">
+                    <div>
+                      <span className="text-lg font-extrabold">권태훈</span>
+                      &nbsp;(68세)
+                    </div>
+                    <div className=" text-[#505050] text-sm whitespace-nowrap	">
+                      인천광역시 미추홀구 용현4동
+                    </div>
+                  </div>
+                  <div className="w-1/4 flex justify-end">
+                    <img
+                      src={next_arrow}
+                      alt="next_arrow"
+                      width={15}
+                      height={15}
+                    />
                   </div>
                 </div>
               </div>
