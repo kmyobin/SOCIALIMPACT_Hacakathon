401fb41486ee98ea5595d5393882a34d89f0409a
taehun0933 <taehun0933@naver.com>
2023-08-24 15:06:32 +0900

    carousel 구현중?_태훈

--- src/components/centerList/Center_1.jsx
+++ src/components/centerList/Center_1.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Center_1() {
   return (
     <section className="w-full h-[814.125px]">
-      <img src="../" alt="" />
+      <img src="../" alt="center1" className="" />
     </section>
   );
 }
