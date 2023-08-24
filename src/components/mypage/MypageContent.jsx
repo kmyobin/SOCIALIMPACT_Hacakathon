c1795259139b36a009369406d6abdaaa0a9bae59
kmyobin <64712993+kmyobin@users.noreply.github.com>
2023-08-24 22:22:33 +0900

    kmyobin commit

--- src/components/mypage/MypageContent.jsx
+++ src/components/mypage/MypageContent.jsx
@@ -9,6 +9,8 @@ import list_4 from "../../assets/images/icons/ent.svg"
 import list_5 from "../../assets/images/icons/list_5.svg"
 import list_6 from "../../assets/images/icons/list_6.svg"
 import list_7 from "../../assets/images/icons/list_7.svg";
+import phone from "../../assets/images/icons/phone.svg";
+
 function MypageContent() {
   const onClickArrow = () => {
     console.log("뒤로가기")
@@ -17,7 +19,7 @@ function MypageContent() {
     <div className="flex h-full w-full flex-wrap justify-center">
       <div className="relative flex h-full w-full flex-col justify-between overflow-hidden bg-[#f6f6f6]">
         <div className="bg-[#5BBDF4] h-full w-full overflow-hidden">
-          <div className="bg-[#5BBDF4] w-full px-7 flex py-2">
+          <div className="bg-[#5BBDF4] w-full h-[20%] px-7 flex py-2">
             <div className="w-1/4 flex justify-start items-center">
               <img
                 src={arrow}
@@ -43,7 +45,7 @@ function MypageContent() {
             </div>
           </div>
           <div
-            className="h-full w-full overflow-hidden p-10 bg-[#f6f6f6] rounded-t-2xl"
+            className="h-[80%] w-full overflow-hidden p-10 bg-[#f6f6f6] rounded-t-2xl"
             style={{ boxShadow: "0px -3px 0px 0px rgba(0,0,0,0.1)" }}
           >
             <div className="flex justify-end pb-2">
@@ -56,48 +58,62 @@ function MypageContent() {
                 </label>
               </div>
             </div>
-            <div className="h-[450px] overflow-y-scroll">
-              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-xl p-3 bg-white flex py-5 ">
-                <div className="w-1/5 justify-center items-center flex">
-                  <img src={list_1} alt="list_1" width={25} height={25} />
-                </div>
-                <div className="w-4/5">시니어 영어 보조 강사 모집합니다.</div>
+            <div className="h-[450px] overflow-y-scroll font-bold">
+              <div className="text-[#505050] text-md font-semibold mb-2 px-2">
+                구직자 필터
               </div>
-              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-xl p-3 bg-white flex py-5">
+              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-2xl p-3 bg-white flex py-5">
                 <div className="w-1/5 justify-center items-center flex">
                   <img src={list_2} alt="list_2" width={25} height={25} />
                 </div>
                 <div className="w-4/5">50~70대</div>
               </div>
-              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-xl p-3 bg-white flex py-5">
+              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-2xl p-3 bg-white flex py-5">
                 <div className="w-1/5 justify-center items-center flex">
                   <img src={list_3} alt="list_3" width={25} height={25} />
                 </div>
                 <div className="w-4/5">인천광역시 미추홀구 용현1동</div>
               </div>
-              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-xl p-3 bg-white flex py-5">
+              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-2xl p-3 bg-white flex py-5">
                 <div className="w-1/5 justify-center items-center flex">
                   <img src={list_4} alt="list_4" width={25} height={25} />
                 </div>
-                <div className="w-4/5">경력 3년 이상</div>
+                <div className="w-4/5">경력 10년 이상</div>
               </div>
-              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-xl p-3 bg-white flex py-5">
+              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-2xl p-3 bg-white flex py-5">
                 <div className="w-1/5 justify-center items-center flex">
                   <img src={list_5} alt="list_5" width={25} height={25} />
                 </div>
-                <div className="w-4/5">교육업, 일본어</div>
+                <div className="w-4/5">학교, 일본어</div>
               </div>
-              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-xl p-3 bg-white flex py-5">
+
+              <div className="text-[#505050] text-md font-semibold mb-2 px-2">
+                채용 정보
+              </div>
+              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-2xl p-3 bg-white flex py-5">
                 <div className="w-1/5 justify-center items-center flex">
                   <img src={list_6} alt="list_6" width={25} height={25} />
                 </div>
                 <div className="w-4/5">9860원(시간)</div>
               </div>
-              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-xl p-3 bg-white flex py-5">
+
+              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-2xl p-3 bg-white flex py-5">
                 <div className="w-1/5 justify-center items-center flex">
                   <img src={list_7} alt="list_7" width={25} height={25} />
                 </div>
                 <div className="w-4/5">6개월, 주말 13시~20시</div>
+              </div>
+              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-2xl p-3 bg-white flex py-5 ">
+                <div className="w-1/5 justify-center items-center flex">
+                  <img src={list_1} alt="list_1" width={25} height={25} />
+                </div>
+                <div className="w-4/5">시니어 영어 보조 강사 모집합니다.</div>
+              </div>
+              <div className="drop-shadow-md shadow-gray-300 mb-5 rounded-2xl p-3 bg-white flex py-5 ">
+                <div className="w-1/5 justify-center items-center flex">
+                  <img src={phone} alt="phone" width={25} height={25} />
+                </div>
+                <div className="w-4/5">010-0000-0000</div>
               </div>
             </div>
           </div>
